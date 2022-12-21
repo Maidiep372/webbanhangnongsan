0423dfc519c73ded322feaa4c491c8f8809b30dc
Maidiep372 <vandiep372@gmail.com>
2022-12-21 20:21:34 +0700

    add file

---
+++ src/main/java/nongsan/webmvc/service/CategoryService.java
@@ -0,0 +1,22 @@
+package nongsan.webmvc.service;
+
+import java.util.List;
+
+import nongsan.webmvc.model.Catalog;
+public interface CategoryService {
+	void insert(Catalog category);
+
+	void edit(Catalog category);
+
+	Catalog get(int id);
+	
+	Catalog get(String name);
+
+	List<Catalog> getAll();
+
+	void delete(String id);
+	
+	List<Catalog> getCateByProduct(int id);
+
+}
+
